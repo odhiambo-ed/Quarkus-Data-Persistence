4f1e8098d8f7e111adaeb318fc0d52fa49348ec0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-15 22:46:34 +0300

    add Project entity

--- src/main/java/org/acme/project/Project.java
+++ src/main/java/org/acme/project/Project.java
@@ -1,14 +1,28 @@
 package org.acme.project;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
+import org.acme.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
 
 @Entity
 @Table(name = "projects", uniqueConstraints = {
         @UniqueConstraint(columnNames = {"name", "user_id"})
 })
 public class Project extends PanacheEntity {
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
 
 }
