48a51cb4486eb60a217f0157ca4a3ef4d3680ef1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-15 16:01:25 +0300

    add User entity

---
+++ src/main/java/org/acme/project/Project.java
@@ -0,0 +1,4 @@
+package org.acme.project;
+
+public class Project {
+}
---
+++ src/main/java/org/acme/task/Task.java
@@ -0,0 +1,9 @@
+package org.acme.task;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Task {
+}
