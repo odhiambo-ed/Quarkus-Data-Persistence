7cf700a6f1206567c2cae9e231263cc98487c496
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-23 22:14:26 +0300

    add project resource

---
+++ src/main/java/org/acme/project/ProjectResource.java
@@ -0,0 +1,47 @@
+package org.acme.project;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import java.util.List;
+
+@Path("/api/v1/projects")
+public class ProjectResource {
+
+    private final ProjectService projectService;
+
+    @Inject
+    public ProjectResource(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GET
+    public Uni<List<Project>> get() {
+        return projectService.listForUser();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @ResponseStatus(201)
+    public Uni<Project> create(Project project) {
+        return projectService.create(project);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    public Uni<Project> update(@PathParam("id") long id, Project project) {
+        project.id = id;
+        return projectService.update(project);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Uni<Void> delete(@PathParam("id") long id) {
+        return projectService.delete(id);
+    }
+
+}
